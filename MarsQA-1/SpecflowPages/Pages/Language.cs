af6650598e77cedb42f47d74ea9e3fa9b9d6273d
archanapatil002 <archanapatil002@gmail.com>
2021-02-25 21:20:54 +1300

    Onbord Tasks Add project files.(only source code)

---
+++ MarsQA-1/SpecflowPages/Pages/Language.cs
@@ -0,0 +1,65 @@
+﻿using MarsQA_1.Feature;
+using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA_1.SpecflowPages.Pages
+{
+    class Language
+    {
+        
+
+        private static IWebElement addnew => Driver.driver.FindElement(By.XPath("(//div[@class='ui teal button '][contains(.,'Add New')])[1]"));
+        private static IWebElement addlanguage => Driver.driver.FindElement(By.XPath("(//input[contains(@type,'text')])[4]"));
+        private static IWebElement languagelevel => Driver.driver.FindElement(By.XPath("//select[contains(@class,'ui dropdown')]"));
+        
+        private static IWebElement addbutton => Driver.driver.FindElement(By.XPath("(//input[contains(@type,'button')])[1]"));
+
+        public static void AddlanguageSteps()
+        {
+            Driver.NavigateUrl();
+            Login login = new Login();
+            addnew.Click();
+            addlanguage.SendKeys("English");
+            SelectElement eselect = new SelectElement(languagelevel);
+            eselect.SelectByIndex(2);
+            addbutton.Click();
+
+        }
+        public static void AddLanguage()
+        {
+            Driver.NavigateUrl();
+
+            //Enter add new
+            Driver.driver.FindElement(By.XPath("(//div[@class='ui teal button '][contains(.,'Add New')])[1]")).Click();
+
+            //Enter language
+            Driver.driver.FindElement(By.XPath("(//input[contains(@type,'text')])[4]")).SendKeys("English");
+
+            //select language level
+            SelectElement eselect = new SelectElement(Driver.driver.FindElement(By.XPath("//select[contains(@class,'ui dropdown')]")));
+            eselect.SelectByIndex(2);
+
+            //Click on Login Button
+            Driver.driver.FindElement(By.XPath("(//input[contains(@type,'button')])[1]")).Click();
+        }
+
+        
+        
+        
+        
+
+        
+        
+           
+        
+
+       
+
+    }
+}
---
+++ MarsQA-1/SpecflowPages/Pages/Profilepage.cs
@@ -0,0 +1,29 @@
+﻿using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA_1.SpecflowPages.Pages
+{
+    class Profilepage
+    {
+        
+        private static IWebElement profile => Driver.driver.FindElement(By.XPath("(//a[contains(@class,'item')])[10]"));
+        //By profile = By.XPath("(//a[contains(@class,'item')])[10]");
+
+    
+    public static void Profile()
+    {
+            Driver.NavigateUrl();
+            Driver.driver.FindElement(By.XPath("(//a[contains(@class,'item')])[10]")).Click();
+
+        }
+        
+            
+        
+
+    }
+}
